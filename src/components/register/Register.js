f7a5aba7a8490722487f727a197be61ec21724e7
deperazar <danielpblog2014@gmail.com>
2022-02-07 08:50:03 -0500

    updating to connect to heroku server

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -22,7 +22,7 @@ class Register extends React.Component {
     }
 
     onSubmitRegister = () => {
-        fetch('http://localhost:3001/register', {
+        fetch('https://immense-falls-35975.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
